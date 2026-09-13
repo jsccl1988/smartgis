d06705de1a9c80b0c380cb992da464a6ce4481d9
jsccl1988 <jsccl1988@.com>
2026-09-13 15:30:38 +0800

    Add shared OGR feature codec for point, line, polygon, and annotation.

--- src/sdb/datasource/gdal/sde_gdal_test.cc
+++ src/sdb/datasource/gdal/sde_gdal_test.cc
@@ -2,8 +2,14 @@
 // All rights reserved.
 
 #include "sdb/datasource/gdal/ogr_connect.h"
+#include "sdb/datasource/gdal/ogr_feature_codec.h"
 
+#include "feature.h"
+#include "geometry.h"
 #include "layer.h"
+
+#include "gdal_priv.h"
+#include "ogrsf_frmts.h"
 
 #include <cstdio>
 #include <cstdlib>
@@ -11,6 +17,9 @@
 #include <string>
 
 using Smt_GIS::SmtDataSourceInfo;
+using Smt_GIS::SmtFeature;
+using Smt_GIS::SmtField;
+using Smt_GIS::SmtFtAnno;
 using Smt_GIS::PROVIDER_ACCESS;
 using Smt_GIS::PROVIDER_GPKG;
 using Smt_GIS::PROVIDER_POSTGRES;
@@ -80,6 +89,56 @@ int main() {
   access.unProvider = PROVIDER_ACCESS;
   expect(sdb::datasource::make_gdal_open_target(access).empty(),
          "ACCESS target empty");
+
+  GDALAllRegister();
+  GDALDriver* mem = GetGDALDriverManager()->GetDriverByName("Memory");
+  expect(mem != nullptr, "Memory OGR driver");
+  if (mem) {
+    GDALDataset* ds = mem->Create("codec_mem", 0, 0, 0, GDT_Unknown, nullptr);
+    expect(ds != nullptr, "Memory datasource");
+    if (ds) {
+      OGRFieldDefn anno("anno", OFTString);
+      OGRFieldDefn color("color", OFTInteger);
+      OGRFieldDefn angle("angle", OFTReal);
+      OGRLayer* lyr = ds->CreateLayer("pts", nullptr, wkbPoint, nullptr);
+      expect(lyr != nullptr, "Memory point layer");
+      if (lyr) {
+        lyr->CreateField(&anno);
+        lyr->CreateField(&color);
+        lyr->CreateField(&angle);
+        SmtFeature smt;
+        smt.SetID(7);
+        smt.SetFeatureType(SmtFtAnno);
+        smt.SetGeometryDirectly(new SmtPoint(1.5, 2.5));
+        SmtField f_anno;
+        f_anno.SetName("anno");
+        f_anno.SetType(SmtString);
+        smt.AddField(f_anno);
+        SmtField f_color;
+        f_color.SetName("color");
+        f_color.SetType(SmtInteger);
+        smt.AddField(f_color);
+        SmtField f_angle;
+        f_angle.SetName("angle");
+        f_angle.SetType(SmtReal);
+        smt.AddField(f_angle);
+        smt.SetFieldValue(smt.GetFieldIndexByName("anno"), "hi");
+        smt.SetFieldValue(smt.GetFieldIndexByName("color"), 9);
+        smt.SetFieldValue(smt.GetFieldIndexByName("angle"), 45.0);
+        OGRFeature ogr(lyr->GetLayerDefn());
+        expect(sdb::datasource::copy_smt_feature_to_ogr(&smt, &ogr),
+               "smt->ogr anno");
+        SmtFeature back;
+        expect(sdb::datasource::copy_ogr_feature_to_smt(&ogr, &back),
+               "ogr->smt anno");
+        const SmtPoint* pt = dynamic_cast<const SmtPoint*>(back.GetGeometryRef());
+        expect(pt && pt->GetX() == 1.5 && pt->GetY() == 2.5, "anno xy");
+        int ai = back.GetFieldIndexByName("anno");
+        expect(ai >= 0, "anno field present");
+      }
+      GDALClose(ds);
+    }
+  }
 
   if (g_fails) {
     std::fprintf(stderr, "%d checks failed\n", g_fails);
